109b245f8810281d45338d909c800413e53339fe
Anton Fernando <anton@z-acceleration.com>
2019-03-07 14:27:09 +0530

    set authority

--- Extensions/ServiceCollectionExtensions.cs
+++ Extensions/ServiceCollectionExtensions.cs
@@ -35,18 +35,19 @@ namespace SprintCrowdBackEnd.Extensions
                 {
                     options.TokenValidationParameters = new TokenValidationParameters
                     {
-                        // Clock skew compensates for server time drift.
-                        // We recommend 5 minutes or less:
-                        ClockSkew = TimeSpan.FromMinutes(5),
-                        IssuerSigningKeys = discoveryDocument.SigningKeys,
-                        RequireSignedTokens = true,
-                        ValidateAudience = true,
-                        ValidAudience = appSettings.Audience,
-                        ValidateIssuer = true,
-                        ValidIssuer = discoveryDocument.Issuer,
-                        RequireExpirationTime = true,
-                        ValidateLifetime = true,
+                    // Clock skew compensates for server time drift.
+                    // We recommend 5 minutes or less:
+                    ClockSkew = TimeSpan.FromMinutes(5),
+                    IssuerSigningKeys = discoveryDocument.SigningKeys,
+                    RequireSignedTokens = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.Audience,
+                    ValidateIssuer = true,
+                    ValidIssuer = discoveryDocument.Issuer,
+                    RequireExpirationTime = true,
+                    ValidateLifetime = true
                     };
+                    options.Authority = appSettings.AuthorizationServer;
                 });
         }
     }
