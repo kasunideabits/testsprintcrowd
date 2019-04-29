63e1f1f970f8cd8e264114880178811556213cb5
Prahveen Thiruchelvam <prahveen@z-acceleration.com>
2019-04-29 10:57:35 +0530

    commit pre hook

---
+++ Scripts/commit_pre_hook.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+MAX_WARNINGS=50
+
+dotnet clean
+count=$( ( dotnet build | grep warning | wc -l ) )
+if [ $count -ge $MAX_WARNINGS ]; then
+   echo '[ERROR] Maximum warning limit exceed, Please fix warnings before push the commits'
+   exit -1;
+fi
+exit 0
