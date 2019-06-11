630390aaba8b208b113df9ad9db29b74167dec23
Anton Thushara <anton@ideabits.se>
2019-06-11 15:12:13 +0530

    add bin to gitignore

---
+++ Web/Sprint/SprintController.cs
@@ -0,0 +1,49 @@
+namespace SprintCrowdBackEnd.Web.Event
+{
+  using System.Threading.Tasks;
+  using Microsoft.AspNetCore.Mvc;
+  using SprintCrowd.Backend.Application;
+  using SprintCrowdBackEnd.Application;
+  using SprintCrowdBackEnd.Domain.ScrowdUser;
+  using SprintCrowdBackEnd.Domain.Sprint;
+  using SprintCrowdBackEnd.Extensions;
+  using SprintCrowdBackEnd.Infrastructure.Persistence.Entities;
+
+  /// <summary>
+  /// event controller
+  /// </summary>
+  public class SprintController : ControllerBase
+  {
+    private ISprintService SprintService;
+    private IUserService UserService;
+    /// <summary>
+    /// intializes an instance of SprintController
+    /// </summary>
+    /// <param name="sprintService">sprint service</param>
+    /// /// <param name="userService">user service</param>
+    public SprintController(ISprintService sprintService, IUserService userService)
+    {
+      this.SprintService = sprintService;
+      this.UserService = userService;
+    }
+
+    /// <summary>
+    /// creates an event
+    /// </summary>
+    /// <param name="sprintInfo">info about the sprint</param>
+    /// <returns></returns>
+    [HttpPost]
+    [Route("create")]
+    public async Task<ResponseObject> CreateEvent([FromBody] SprintModel sprintInfo)
+    {
+      User user = await this.User.GetUser(this.UserService);
+      var result = this.SprintService.CreateNewSprint(sprintInfo, user);
+
+      return new ResponseObject()
+      {
+        StatusCode = (int)ApplicationResponseCode.Success,
+        Data = result
+      };
+    }
+  }
+}
