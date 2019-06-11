2cfe02e1f6eaee84958e4b05d0cc6bef8626a896
Anton Thushara <anton@ideabits.se>
2019-06-11 15:04:08 +0530

    add sprint status and sprint type

---
+++ Application/SprintType.cs
@@ -0,0 +1,13 @@
+namespace SprintCrowdBackEnd.Application
+{
+  /// <summary>
+  /// Types of sprints
+  /// </summary>
+  public enum SprintType
+  {
+    ///public sprint
+    PublicSprint = 0,
+    ///private sprint
+    PrivateSprint = 1
+  }
+}
