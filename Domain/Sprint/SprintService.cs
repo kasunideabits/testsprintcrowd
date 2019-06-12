5bb2f925189424a08bdc73af3dc4fd0a066c1be1
Anton Thushara <anton@ideabits.se>
2019-06-12 21:23:11 +0530

    test for get all sprints

--- Domain/Sprint/SprintService.cs
+++ Domain/Sprint/SprintService.cs
@@ -1,93 +1,93 @@
 ﻿namespace SprintCrowd.BackEnd.Domain.Sprint
 {
-    using System.Collections.Generic;
-    using System.Threading.Tasks;
-    using SprintCrowd.BackEnd.Application;
-    using SprintCrowd.BackEnd.Infrastructure.Persistence.Entities;
+  using System.Collections.Generic;
+  using System.Threading.Tasks;
+  using SprintCrowd.BackEnd.Application;
+  using SprintCrowd.BackEnd.Infrastructure.Persistence.Entities;
+
+  /// <summary>
+  ///   /// Sprint service
+  /// </summary>
+  public class SprintService : ISprintService
+  {
+    private ISprintRepo SprintRepo;
+    /// <summary>
+    /// initializes an instance of SprintService
+    /// </summary>
+    /// <param name="sprintRepo">sprint repository</param>
+    public SprintService(ISprintRepo sprintRepo)
+    {
+      this.SprintRepo = sprintRepo;
+    }
 
     /// <summary>
-    ///   /// Sprint service
+    /// Get all events
     /// </summary>
-    public class SprintService : ISprintService
+    /// <returns>Available all events</returns>
+    public async Task<List<Sprint>> GetAll()
     {
-        private ISprintRepo SprintRepo;
-        /// <summary>
-        /// initializes an instance of SprintService
-        /// </summary>
-        /// <param name="sprintRepo">sprint repository</param>
-        public SprintService(ISprintRepo sprintRepo)
-        {
-            this.SprintRepo = sprintRepo;
-        }
-
-        /// <summary>
-        /// Get all events
-        /// </summary>
-        /// <returns>Available all events</returns>
-        public async Task<List<Sprint>> GetAll()
-        {
-            return await this.SprintRepo.GetAllEvents();
-        }
-
-        /// <summary>
-        /// Update instance of SprintService
-        /// </summary>
-        /// <param name="sprintData">sprint repository</param>
-        public async Task<Sprint> UpdateSprint(SprintModel sprintData)
-        {
-
-            Sprint UpdateSprint = new Sprint();
-            UpdateSprint.Id = sprintData.Id;
-
-            var sprintAavail = await this.SprintRepo.GetSprint(UpdateSprint.Id);
-            sprintAavail.Name = sprintData.Name;
-            sprintAavail.Distance = sprintData.Distance;
-            sprintAavail.StartDateTime = sprintData.StartTime;
-            sprintAavail.Type = sprintData.SprintType;
-            sprintAavail.LocationProvided = sprintData.LocationProvided;
-            sprintAavail.Lattitude = sprintData.Lattitude;
-            sprintAavail.Longitutude = sprintData.Longitutude;
-            sprintAavail.NumberOfParticipants = sprintData.NumberOfParticipants;
-            var value = sprintAavail.Id;
-            if (sprintAavail != null)
-            {
-                Sprint sprint = await this.SprintRepo.UpdateSprint(sprintAavail);
-                if (sprint != null)
-                {
-                    this.SprintRepo.SaveChanges();
-                }
-                return sprint;
-            }
-
-            return null;
-        }
-        /// <summary>
-        /// creates a new sprint
-        /// </summary>
-        /// <param name="sprintInfo">info about the sprint</param>
-        /// /// <param name="ownerOfSprint">user who created the sprint</param>
-        /// <returns>created sprint</returns>
-        public async Task<Sprint> CreateNewSprint(SprintModel sprintInfo, User ownerOfSprint)
-        {
-            Sprint sprintToBeCreated = new Sprint();
-            sprintToBeCreated.CreatedBy = ownerOfSprint;
-            sprintToBeCreated.Type = sprintInfo.SprintType;
-            sprintToBeCreated.LocationProvided = sprintInfo.LocationProvided;
-            sprintToBeCreated.Lattitude = sprintInfo.Lattitude;
-            sprintToBeCreated.Longitutude = sprintInfo.Longitutude;
-            sprintToBeCreated.Name = sprintInfo.Name;
-            sprintToBeCreated.StartDateTime = sprintInfo.StartTime;
-            sprintToBeCreated.Status = (int)SprintStatus.NOTSTARTEDYET;
-            sprintToBeCreated.Distance = sprintInfo.Distance;
-            sprintToBeCreated.NumberOfParticipants = sprintInfo.NumberOfParticipants;
-            Sprint sprint = await this.SprintRepo.AddSprint(sprintToBeCreated);
-            if (sprint != null)
-            {
-                this.SprintRepo.SaveChanges();
-            }
-
-            return sprint;
-        }
-
+      return await this.SprintRepo.GetAllEvents();
     }
+
+    /// <summary>
+    /// Update instance of SprintService
+    /// </summary>
+    /// <param name="sprintData">sprint repository</param>
+    public async Task<Sprint> UpdateSprint(SprintModel sprintData)
+    {
+
+      Sprint UpdateSprint = new Sprint();
+      UpdateSprint.Id = sprintData.Id;
+
+      var sprintAavail = await this.SprintRepo.GetSprint(UpdateSprint.Id);
+      sprintAavail.Name = sprintData.Name;
+      sprintAavail.Distance = sprintData.Distance;
+      sprintAavail.StartDateTime = sprintData.StartTime;
+      sprintAavail.Type = sprintData.SprintType;
+      sprintAavail.LocationProvided = sprintData.LocationProvided;
+      sprintAavail.Lattitude = sprintData.Lattitude;
+      sprintAavail.Longitutude = sprintData.Longitutude;
+      sprintAavail.NumberOfParticipants = sprintData.NumberOfParticipants;
+      var value = sprintAavail.Id;
+      if (sprintAavail != null)
+      {
+        Sprint sprint = await this.SprintRepo.UpdateSprint(sprintAavail);
+        if (sprint != null)
+        {
+          this.SprintRepo.SaveChanges();
+        }
+        return sprint;
+      }
+
+      return null;
+    }
+    /// <summary>
+    /// creates a new sprint
+    /// </summary>
+    /// <param name="sprintInfo">info about the sprint</param>
+    /// /// <param name="ownerOfSprint">user who created the sprint</param>
+    /// <returns>created sprint</returns>
+    public async Task<Sprint> CreateNewSprint(SprintModel sprintInfo, User ownerOfSprint)
+    {
+      Sprint sprintToBeCreated = new Sprint();
+      sprintToBeCreated.CreatedBy = ownerOfSprint;
+      sprintToBeCreated.Type = sprintInfo.SprintType;
+      sprintToBeCreated.LocationProvided = sprintInfo.LocationProvided;
+      sprintToBeCreated.Lattitude = sprintInfo.Lattitude;
+      sprintToBeCreated.Longitutude = sprintInfo.Longitutude;
+      sprintToBeCreated.Name = sprintInfo.Name;
+      sprintToBeCreated.StartDateTime = sprintInfo.StartTime;
+      sprintToBeCreated.Status = (int)SprintStatus.NOTSTARTEDYET;
+      sprintToBeCreated.Distance = sprintInfo.Distance;
+      sprintToBeCreated.NumberOfParticipants = sprintInfo.NumberOfParticipants;
+      Sprint sprint = await this.SprintRepo.AddSprint(sprintToBeCreated);
+      if (sprint != null)
+      {
+        this.SprintRepo.SaveChanges();
+      }
+
+      return sprint;
+    }
+
+  }
 }
--- Web/Sprint/SprintController.cs
+++ Web/Sprint/SprintController.cs
@@ -1,81 +1,81 @@
 ﻿namespace SprintCrowd.BackEnd.Web.Event
 {
-    using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Authorization;
-    using Microsoft.AspNetCore.Mvc;
-    using SprintCrowd.BackEnd.Application;
-    using SprintCrowd.BackEnd.Domain.ScrowdUser;
-    using SprintCrowd.BackEnd.Domain.Sprint;
-    using SprintCrowd.BackEnd.Extensions;
-    using SprintCrowd.BackEnd.Infrastructure.Persistence.Entities;
+  using System.Threading.Tasks;
+  using Microsoft.AspNetCore.Authorization;
+  using Microsoft.AspNetCore.Mvc;
+  using SprintCrowd.BackEnd.Application;
+  using SprintCrowd.BackEnd.Domain.ScrowdUser;
+  using SprintCrowd.BackEnd.Domain.Sprint;
+  using SprintCrowd.BackEnd.Extensions;
+  using SprintCrowd.BackEnd.Infrastructure.Persistence.Entities;
+
+  /// <summary>
+  /// event controller
+  /// </summary>
+  [Route("[controller]")]
+  [ApiController]
+  [Authorize]
+
+  public class SprintController : ControllerBase
+  {
+    private ISprintService SprintService;
+    private IUserService UserService;
+    /// <summary>
+    /// intializes an instance of SprintController
+    /// </summary>
+    /// <param name="sprintService">sprint service</param>
+    /// /// <param name="userService">user service</param>
+    public SprintController(ISprintService sprintService, IUserService userService)
+    {
+      this.SprintService = sprintService;
+      this.UserService = userService;
+    }
 
     /// <summary>
-    /// event controller
+    /// Get all events
     /// </summary>
-    [Route("[controller]")]
-    [ApiController]
-    [Authorize]
-
-    public class SprintController : ControllerBase
+    /// <returns>All events available in database</returns>
+    [HttpGet("get")]
+    [ProducesResponseType(typeof(ResponseObject), 200)]
+    public async Task<ResponseObject> GetAllEvents()
     {
-        private ISprintService SprintService;
-        private IUserService UserService;
-        /// <summary>
-        /// intializes an instance of SprintController
-        /// </summary>
-        /// <param name="sprintService">sprint service</param>
-        /// /// <param name="userService">user service</param>
-        public SprintController(ISprintService sprintService, IUserService userService)
-        {
-            this.SprintService = sprintService;
-            this.UserService = userService;
-        }
-
-        /// <summary>
-        /// Get all events
-        /// </summary>
-        /// <returns>All events available in database</returns>
-        [HttpGet("get")]
-        [ProducesResponseType(typeof(ResponseObject), 200)]
-        public IActionResult GetAllEvents()
-        {
-            ResponseObject response = new ResponseObject()
-            {
-                StatusCode = (int)ApplicationResponseCode.Success,
-                Data = this.SprintService.GetAll()
-            };
-            return this.Ok(response);
-        }
-
-        /// <summary>
-        /// creates an event
-        /// </summary>
-        /// <param name="sprintInfo">info about the sprint</param>
-        /// <returns></returns>
-        [HttpPost]
-        [Route("create")]
-        public async Task<ResponseObject> CreateEvent([FromBody] SprintModel sprintInfo)
-        {
-            User user = await this.User.GetUser(this.UserService);
-            var result = await this.SprintService.CreateNewSprint(sprintInfo, user);
-
-            return new ResponseObject()
-            {
-                StatusCode = (int)ApplicationResponseCode.Success,
-                    Data = result
-            };
-        }
-
-        /// <summary>
-        /// update sprint
-        /// </summary>
-        [HttpPut]
-        [Route("update")]
-        public async Task<ResponseObject> UpdateEvent([FromBody] SprintModel SprintData)
-        {
-            Sprint sprint = await this.SprintService.UpdateSprint(SprintData);
-
-            return new ResponseObject { StatusCode = (int)ApplicationResponseCode.Success, Data = sprint };
-        }
+      ResponseObject response = new ResponseObject()
+      {
+        StatusCode = (int)ApplicationResponseCode.Success,
+        Data = await this.SprintService.GetAll()
+      };
+      return response;
     }
+
+    /// <summary>
+    /// creates an event
+    /// </summary>
+    /// <param name="sprintInfo">info about the sprint</param>
+    /// <returns></returns>
+    [HttpPost]
+    [Route("create")]
+    public async Task<ResponseObject> CreateEvent([FromBody] SprintModel sprintInfo)
+    {
+      User user = await this.User.GetUser(this.UserService);
+      var result = await this.SprintService.CreateNewSprint(sprintInfo, user);
+
+      return new ResponseObject()
+      {
+        StatusCode = (int)ApplicationResponseCode.Success,
+        Data = result
+      };
+    }
+
+    /// <summary>
+    /// update sprint
+    /// </summary>
+    [HttpPut]
+    [Route("update")]
+    public async Task<ResponseObject> UpdateEvent([FromBody] SprintModel SprintData)
+    {
+      Sprint sprint = await this.SprintService.UpdateSprint(SprintData);
+
+      return new ResponseObject { StatusCode = (int)ApplicationResponseCode.Success, Data = sprint };
+    }
+  }
 }
