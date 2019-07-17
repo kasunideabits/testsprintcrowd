f863f6a4864aaba5e340f1cbad32892475c20019
Anton Thushara <anton@ideabits.se>
2019-07-17 10:22:58 +0530

    add relation sprint to sprint participant

--- Infrastructure/Persistence/Configuration/SprintParticipantConfig.cs
+++ Infrastructure/Persistence/Configuration/SprintParticipantConfig.cs
@@ -1,23 +1,24 @@
 ﻿namespace SprintCrowd.BackEnd.Infrastructure.Persistence.Configuration
 {
-  using System;
-  using Microsoft.EntityFrameworkCore.Metadata.Builders;
-  using Microsoft.EntityFrameworkCore;
-  using SprintCrowd.BackEnd.Infrastructure.Persistence.Entities;
+    using System;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Microsoft.EntityFrameworkCore;
+    using SprintCrowd.BackEnd.Infrastructure.Persistence.Entities;
 
-  /// <summary>
-  /// Entity configuration for SprintParticipant table
-  /// </summary>
-  public class SprintParticipantConfig : IEntityTypeConfiguration<SprintParticipant>
-  {
     /// <summary>
-    /// Configure table SprintParticipant
+    /// Entity configuration for SprintParticipant table
     /// </summary>
-    /// <param name="builder">entity builder instance</param>
-    public void Configure(EntityTypeBuilder<SprintParticipant> builder)
+    public class SprintParticipantConfig : IEntityTypeConfiguration<SprintParticipant>
     {
-      builder.HasOne(ep => ep.User);
-      builder.Property<DateTime>("LastUpdated");
+        /// <summary>
+        /// Configure table SprintParticipant
+        /// </summary>
+        /// <param name="builder">entity builder instance</param>
+        public void Configure(EntityTypeBuilder<SprintParticipant> builder)
+        {
+            builder.HasOne(ep => ep.User);
+            builder.HasOne(ep => ep.Sprint);
+            builder.Property<DateTime>("LastUpdated");
+        }
     }
-  }
 }
