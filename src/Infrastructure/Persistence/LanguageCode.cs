5af6bb1eac860b6ac9a4ff8c50550877a12a7875
prahveen <prahveent@gmail.com>
2019-07-22 17:25:21 +0530

    add language preference

---
+++ src/Infrastructure/Persistence/LanguageCode.cs
@@ -0,0 +1,18 @@
+namespace SprintCrowd.BackEnd.Infrastructure.Persistence
+{
+    /// <summary>
+    /// ISO 639-1 Code for lanugages
+    /// </summary>
+    public class LanguageCode
+    {
+        /// <summary>
+        /// English language code
+        /// </summary>
+        public const string English = "en";
+
+        /// <summary>
+        /// Swedish language code
+        /// </summary>
+        public const string Swedish = "sv";
+    }
+}
--- src/Web/Account/RegisterModel.cs
+++ src/Web/Account/RegisterModel.cs
@@ -1,22 +1,30 @@
 namespace SprintCrowd.BackEnd.Web.Account
 {
-  /// <summary>
-  /// model for holding register data send to sprintcrowdbackend from mobile
-  /// for registering user.
-  /// </summary>
-  public class RegisterModel
-  {
+    using SprintCrowd.BackEnd.Infrastructure.Persistence;
+
     /// <summary>
-    /// access token
-    /// TODO: for now only facebook access token, if future we may add login provider type
-    /// to support google and other third party logins
+    /// model for holding register data send to sprintcrowdbackend from mobile
+    /// for registering user.
     /// </summary>
-    /// <value>facebook access token.</value>
-    public string AccessToken { get; set; }
-    /// <summary>
-    /// gets or sets value
-    /// </summary>
-    /// <value>email of the user.</value>
-    public string Email { get; set; }
-  }
+    public class RegisterModel
+    {
+        /// <summary>
+        /// access token
+        /// TODO: for now only facebook access token, if future we may add login provider type
+        /// to support google and other third party logins
+        /// </summary>
+        /// <value>facebook access token.</value>
+        public string AccessToken { get; set; }
+        /// <summary>
+        /// gets or sets value
+        /// </summary>
+        /// <value>email of the user.</value>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// <see cref="LanguageCode"> Language preference for user </see>
+        /// </summary>
+        /// <value>language code</value>
+        public string LanguagePreference { get; set; }
+    }
 }
