f66ce08467f29e02138d43d29209dbff4a9e4d69
prahveen <prahveent@gmail.com>
2019-10-16 14:36:44 +0530

    add color code

--- src/Infrastructure/Persistence/Entities/User.cs
+++ src/Infrastructure/Persistence/Entities/User.cs
@@ -1,136 +1,143 @@
 namespace SprintCrowd.BackEnd.Infrastructure.Persistence.Entities
 {
-  using System.Collections.Generic;
+    using System.Collections.Generic;
+    using SprintCrowd.BackEnd.Application;
 
-  /// <summary>
-  /// User model.
-  /// </summary>
-  public class User
-  {
     /// <summary>
-    /// Initializes a new instance of the <see cref="User"/> class.
+    /// User model.
     /// </summary>
-    public User()
+    public class User
     {
-      this.AccessToken = new AccessToken();
+        /// <summary>
+        /// Initializes a new instance of the <see cref="User"/> class.
+        /// </summary>
+        public User()
+        {
+            this.AccessToken = new AccessToken();
+        }
+
+        /// <summary>
+        /// gets or set value.
+        /// </summary>
+        /// <value>unique id for the user.</value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// gets or set value.
+        /// </summary>
+        /// <value>type of the user.facebook or other..</value>
+        public int UserType { get; set; }
+
+        /// <summary>
+        /// gets or set value.
+        /// </summary>
+        /// <value>facebook user id.</value>
+        public string FacebookUserId { get; set; }
+
+        /// <summary>
+        /// gets or set value.
+        /// </summary>
+        /// <value>email of the user.</value>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// gets or set value.
+        /// </summary>
+        /// <value>name of the user.</value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// gets or set value.
+        /// </summary>
+        /// <value>profile picture url of the user.</value>
+        public string ProfilePicture { get; set; }
+
+        /// <summary>
+        /// Unique code for user
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// gets or set value.
+        /// </summary>
+        /// <value>list of sptints the user has participated and participating on.</value>
+        public List<Sprint> Sprint { get; set; }
+
+        /// <summary>
+        /// gets or set value.
+        /// </summary>
+        /// <value>access token.</value>
+        public AccessToken AccessToken { get; set; }
+
+        /// <summary>
+        /// Language Preference for user <see cref="LanguageCode"> Lanugage code </see>
+        /// </summary>
+        /// <value> Lanugage code </value>
+        public string LanguagePreference { get; set; } = LanguageCode.English;
+
+        /// <summary>
+        /// gets or set value.
+        /// </summary>
+        /// <value>country name of the user</value>
+        public string Country { get; set; }
+
+        /// <summary>
+        /// gets or set value.
+        /// </summary>
+        /// <value>country code of the user</value>
+        public string CountryCode { get; set; }
+
+        /// <summary>
+        /// gets or set value.
+        /// </summary>
+        /// <value>city name of the user</value>
+        public string City { get; set; }
+
+        /// <summary>
+        /// Color code for user <see cref="UserColorCode"> color codes </see>
+        /// </summary>
+        /// <value></value>
+        public string ColorCode { get; set; }
+
+        /// <summary>
+        /// Gets or set reference for participates
+        /// </summary>
+        public List<SprintParticipant> Participates { get; set; }
+
+        /// <summary>
+        /// Gets or set Notification reference for sender
+        /// </summary>
+        public virtual List<Notification> SenderNotification { get; set; }
+
+        /// <summary>
+        /// Gets or set Notification reference for receiver
+        /// </summary>
+        public virtual List<Notification> ReceiverNotification { get; set; }
+
+        /// <summary>
+        /// Gets or set reference for achievements
+        /// </summary>
+        public virtual List<Achievement> Achievements { get; set; }
+
+        /// <summary>
+        /// Gets or set inviter rederence
+        /// </summary>
+        public virtual List<SprintInvite> Inviter { get; set; }
+
+        /// <summary>
+        /// Gets or set invitee reference
+        /// </summary>
+        public virtual List<SprintInvite> Invitee { get; set; }
+
+        /// <summary>
+        /// Gets or set friend reference
+        /// </summary>
+        public virtual List<Friend> friendsAccepted { get; set; }
+
+        /// <summary>
+        /// Gets or set friend reference
+        /// </summary>
+        public virtual List<Friend> friendsShared { get; set; }
+
     }
-
-    /// <summary>
-    /// gets or set value.
-    /// </summary>
-    /// <value>unique id for the user.</value>
-    public int Id { get; set; }
-
-    /// <summary>
-    /// gets or set value.
-    /// </summary>
-    /// <value>type of the user.facebook or other..</value>
-    public int UserType { get; set; }
-
-    /// <summary>
-    /// gets or set value.
-    /// </summary>
-    /// <value>facebook user id.</value>
-    public string FacebookUserId { get; set; }
-
-    /// <summary>
-    /// gets or set value.
-    /// </summary>
-    /// <value>email of the user.</value>
-    public string Email { get; set; }
-
-    /// <summary>
-    /// gets or set value.
-    /// </summary>
-    /// <value>name of the user.</value>
-    public string Name { get; set; }
-
-    /// <summary>
-    /// gets or set value.
-    /// </summary>
-    /// <value>profile picture url of the user.</value>
-    public string ProfilePicture { get; set; }
-
-    /// <summary>
-    /// Unique code for user
-    /// </summary>
-    public string Code { get; set; }
-
-    /// <summary>
-    /// gets or set value.
-    /// </summary>
-    /// <value>list of sptints the user has participated and participating on.</value>
-    public List<Sprint> Sprint { get; set; }
-
-    /// <summary>
-    /// gets or set value.
-    /// </summary>
-    /// <value>access token.</value>
-    public AccessToken AccessToken { get; set; }
-
-    /// <summary>
-    /// Language Preference for user <see cref="LanguageCode"> Lanugage code </see>
-    /// </summary>
-    /// <value> Lanugage code </value>
-    public string LanguagePreference { get; set; } = LanguageCode.English;
-
-    /// <summary>
-    /// gets or set value.
-    /// </summary>
-    /// <value>country name of the user</value>
-    public string Country { get; set; }
-
-    /// <summary>
-    /// gets or set value.
-    /// </summary>
-    /// <value>country code of the user</value>
-    public string CountryCode { get; set; }
-
-    /// <summary>
-    /// gets or set value.
-    /// </summary>
-    /// <value>city name of the user</value>
-    public string City { get; set; }
-
-    /// <summary>
-    /// Gets or set reference for participates
-    /// </summary>
-    public List<SprintParticipant> Participates { get; set; }
-
-    /// <summary>
-    /// Gets or set Notification reference for sender
-    /// </summary>
-    public virtual List<Notification> SenderNotification { get; set; }
-
-    /// <summary>
-    /// Gets or set Notification reference for receiver
-    /// </summary>
-    public virtual List<Notification> ReceiverNotification { get; set; }
-
-    /// <summary>
-    /// Gets or set reference for achievements
-    /// </summary>
-    public virtual List<Achievement> Achievements { get; set; }
-
-    /// <summary>
-    /// Gets or set inviter rederence
-    /// </summary>
-    public virtual List<SprintInvite> Inviter { get; set; }
-
-    /// <summary>
-    /// Gets or set invitee reference
-    /// </summary>
-    public virtual List<SprintInvite> Invitee { get; set; }
-
-    /// <summary>
-    /// Gets or set friend reference
-    /// </summary>
-    public virtual List<Friend> friendsAccepted { get; set; }
-
-    /// <summary>
-    /// Gets or set friend reference
-    /// </summary>
-    public virtual List<Friend> friendsShared { get; set; }
-
-  }
 }
---
+++ src/Migrations/20191016090023_UserColorCode.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SprintCrowdBackEnd.Migrations
+{
+    public partial class UserColorCode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "color_code",
+                table: "user",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "color_code",
+                table: "user");
+        }
+    }
+}
