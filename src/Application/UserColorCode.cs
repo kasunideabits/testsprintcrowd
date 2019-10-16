7033767ac33af7c7e58f4e1567dedc8bc020c85f
prahveen <prahveent@gmail.com>
2019-10-16 14:54:36 +0530

    generate random color code while register

---
+++ src/Application/UserColorCode.cs
@@ -0,0 +1,40 @@
+namespace SprintCrowd.BackEnd.Application
+{
+    using System.Collections.Generic;
+    using System;
+
+    /// <summary>
+    /// Generate random user color code
+    /// </summary>
+    public class UserColorCode
+    {
+        /// <summary>
+        /// Initialize class
+        /// </summary>
+        public UserColorCode()
+        {
+            this.InitializeColors();
+        }
+
+        private const string c8e4f5 = "#c8e4f5";
+        private const string c8f1f4 = "#c8f1f4";
+        private const string c8f4dc = "#c8e4f5";
+        private List<string> Colors { get; set; }
+
+        /// <summary>
+        /// Pick random color
+        /// </summary>
+        /// <returns>color hexa code</returns>
+        public string PickColor()
+        {
+            var randomIndex = new Random().Next(0, this.Colors.Count);
+            return this.Colors [randomIndex];
+        }
+
+        private void InitializeColors()
+        {
+            this.Colors = new List<string>();
+            this.Colors.AddRange(new string [] { c8e4f5, c8f1f4, c8f4dc });
+        }
+    }
+}
--- src/Domain/ScrowdUser/UserRepo.cs
+++ src/Domain/ScrowdUser/UserRepo.cs
@@ -1,153 +1,154 @@
 namespace SprintCrowd.BackEnd.Domain.ScrowdUser
 {
-  using System.Threading.Tasks;
-  using Microsoft.EntityFrameworkCore;
-  using Microsoft.Extensions.Options;
-  using RestSharp;
-  using SprintCrowd.BackEnd.Application;
-  using SprintCrowd.BackEnd.Infrastructure.Persistence.Entities;
-  using SprintCrowd.BackEnd.Infrastructure.Persistence;
-  using SprintCrowd.BackEnd.Models;
-  using SprintCrowd.BackEnd.Web.Account;
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Options;
+    using RestSharp;
+    using SprintCrowd.BackEnd.Application;
+    using SprintCrowd.BackEnd.Infrastructure.Persistence.Entities;
+    using SprintCrowd.BackEnd.Infrastructure.Persistence;
+    using SprintCrowd.BackEnd.Models;
+    using SprintCrowd.BackEnd.Web.Account;
 
-  /// ONLY REPOSITORIES WILL ACCESS THE DATABASE
-  /// NO DIRECT ACCESS FROM SERVICES OR CONTROLLERS ALLOWED.
-  /// <summary>
-  ///  user repository for getting and modifing users.
-  /// </summary>
-  public class UserRepo : IUserRepo
-  {
+    /// ONLY REPOSITORIES WILL ACCESS THE DATABASE
+    /// NO DIRECT ACCESS FROM SERVICES OR CONTROLLERS ALLOWED.
     /// <summary>
-    /// Initializes a new instance of the <see cref="UserRepo"/> class.
+    ///  user repository for getting and modifing users.
     /// </summary>
-    /// <param name="dbContext">db connection. dependecy injected.</param>
-    /// <param name="appSettings">config settings of the app.</param>
-    public UserRepo(ScrowdDbContext dbContext, IOptions<AppSettings> appSettings)
+    public class UserRepo : IUserRepo
     {
-      this.dbContext = dbContext;
-      this.appSettings = appSettings.Value;
-      this.restClient = new RestClient(this.appSettings.AuthorizationServer);
-    }
-
-    private readonly ScrowdDbContext dbContext;
-    private readonly RestClient restClient;
-    private readonly AppSettings appSettings;
-
-    /// <summary>
-    /// Gets user info
-    /// </summary>
-    /// <param name="userId">user id for lookup</param>
-    /// <returns><see cref="User"> user info details </see></returns>
-    public Task<User> GetUser(int userId)
-    {
-      return this.dbContext.User.FirstOrDefaultAsync(u => u.Id == userId);
-    }
-
-    /// <summary>
-    /// Returns the user with given facebook user id.
-    /// In the future if more user types are nedded
-    /// room for improvement.
-    /// </summary>
-    /// <param name="userId">Facebook user id.</param>
-    public async Task<User> GetFacebookUser(string userId)
-    {
-      return await this.dbContext.User.FirstOrDefaultAsync(u => u.FacebookUserId.Equals(userId));
-    }
-
-    /// <summary>
-    /// get user by user id
-    /// </summary>
-    /// <param name="userId">id of the user</param>
-    /// <returns>user</returns>
-    public async Task<User> GetUserById(int userId)
-    {
-      return await this.dbContext.User.FirstOrDefaultAsync(u => u.Id.Equals(userId));
-    }
-
-    /// <summary>
-    /// register user in identity server and register user in db.
-    /// </summary>
-    /// <param name="registerData">registeration data.</param>
-    public async Task<User> RegisterUser(RegisterModel registerData)
-    {
-      RestRequest request = new RestRequest("Account/RegisterUser", Method.POST);
-      request.AddJsonBody(new { UserType = (int)UserType.Facebook, Email = string.Empty, Token = registerData.AccessToken });
-      // user returned by the identity server
-      IdentityServerRegisterResponse registerResponse = await this.restClient.PostAsync<IdentityServerRegisterResponse>(request);
-      if (registerResponse.StatusCode != 200)
-      {
-        // Oh ohh, error occured during registeration in identity server
-        throw new ApplicationException(
-            registerResponse.StatusCode ?? (int)ApplicationErrorCode.UnknownError,
-            registerResponse.ErrorDescription ?? "Failed to register user in identity server");
-      }
-
-      var exist = await this.dbContext.User.FirstOrDefaultAsync(u => u.Email.Equals(registerData.Email));
-
-      var code = SCrowdUniqueKey.GetUniqueKey();
-      var codeExist = await this.dbContext.User.FirstOrDefaultAsync(u => u.Code.Equals(code));
-
-      while (codeExist != null)
-      {
-        code = SCrowdUniqueKey.GetUniqueKey();
-        codeExist = await this.dbContext.User.FirstOrDefaultAsync(u => u.Code.Equals(code));
-      }
-
-      if (exist == null)
-      {
-        User user = new User();
-        user.Email = registerData.Email;
-        user.FacebookUserId = registerResponse.Data.UserId;
-        user.Name = registerResponse.Data.Name;
-        user.UserType = (int)UserType.Facebook;
-        user.ProfilePicture = registerResponse.Data.ProfilePicture;
-        user.AccessToken.Token = registerData.AccessToken;
-        user.Country = registerResponse.Data.Country;
-        user.City = registerResponse.Data.City;
-        user.CountryCode = registerResponse.Data.CountryCode;
-        user.Code = code;
-        var FbUser = await this.dbContext.User.AddAsync(user);
-        return FbUser.Entity;
-      }
-      return exist;
-
-    }
-
-    /// <summary>
-    /// commit and save changes to the db
-    /// only call this from the service, DO NOT CALL FROM REPO ITSELF
-    /// Unit of work methology.
-    /// </summary>
-    public void SaveChanges()
-    {
-      this.dbContext.SaveChanges();
-    }
-
-    /// <summary>
-    /// saves fcm token
-    /// </summary>
-    /// <param name="userId">id of the user</param>
-    /// <param name="fcmToken">fmc token</param>
-    /// <returns>async task</returns>
-    public async Task SaveFcmToken(int userId, string fcmToken)
-    {
-      FirebaseMessagingToken existingToken = await this.dbContext.FirebaseToken.FirstOrDefaultAsync(token => token.User.Id == userId);
-      if (existingToken == null)
-      {
-        // no token yet saved, insert
-        FirebaseMessagingToken newFcmToken = new FirebaseMessagingToken()
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserRepo"/> class.
+        /// </summary>
+        /// <param name="dbContext">db connection. dependecy injected.</param>
+        /// <param name="appSettings">config settings of the app.</param>
+        public UserRepo(ScrowdDbContext dbContext, IOptions<AppSettings> appSettings)
         {
-          User = await this.GetUserById(userId),
-          Token = fcmToken,
-        };
-        await this.dbContext.FirebaseToken.AddAsync(newFcmToken);
-      }
-      else
-      {
-        // update
-        existingToken.Token = fcmToken;
-        this.dbContext.FirebaseToken.Update(existingToken);
-      }
+            this.dbContext = dbContext;
+            this.appSettings = appSettings.Value;
+            this.restClient = new RestClient(this.appSettings.AuthorizationServer);
+        }
+
+        private readonly ScrowdDbContext dbContext;
+        private readonly RestClient restClient;
+        private readonly AppSettings appSettings;
+
+        /// <summary>
+        /// Gets user info
+        /// </summary>
+        /// <param name="userId">user id for lookup</param>
+        /// <returns><see cref="User"> user info details </see></returns>
+        public Task<User> GetUser(int userId)
+        {
+            return this.dbContext.User.FirstOrDefaultAsync(u => u.Id == userId);
+        }
+
+        /// <summary>
+        /// Returns the user with given facebook user id.
+        /// In the future if more user types are nedded
+        /// room for improvement.
+        /// </summary>
+        /// <param name="userId">Facebook user id.</param>
+        public async Task<User> GetFacebookUser(string userId)
+        {
+            return await this.dbContext.User.FirstOrDefaultAsync(u => u.FacebookUserId.Equals(userId));
+        }
+
+        /// <summary>
+        /// get user by user id
+        /// </summary>
+        /// <param name="userId">id of the user</param>
+        /// <returns>user</returns>
+        public async Task<User> GetUserById(int userId)
+        {
+            return await this.dbContext.User.FirstOrDefaultAsync(u => u.Id.Equals(userId));
+        }
+
+        /// <summary>
+        /// register user in identity server and register user in db.
+        /// </summary>
+        /// <param name="registerData">registeration data.</param>
+        public async Task<User> RegisterUser(RegisterModel registerData)
+        {
+            RestRequest request = new RestRequest("Account/RegisterUser", Method.POST);
+            request.AddJsonBody(new { UserType = (int)UserType.Facebook, Email = string.Empty, Token = registerData.AccessToken });
+            // user returned by the identity server
+            IdentityServerRegisterResponse registerResponse = await this.restClient.PostAsync<IdentityServerRegisterResponse>(request);
+            if (registerResponse.StatusCode != 200)
+            {
+                // Oh ohh, error occured during registeration in identity server
+                throw new ApplicationException(
+                    registerResponse.StatusCode ?? (int)ApplicationErrorCode.UnknownError,
+                    registerResponse.ErrorDescription ?? "Failed to register user in identity server");
+            }
+
+            var exist = await this.dbContext.User.FirstOrDefaultAsync(u => u.Email.Equals(registerData.Email));
+
+            var code = SCrowdUniqueKey.GetUniqueKey();
+            var codeExist = await this.dbContext.User.FirstOrDefaultAsync(u => u.Code.Equals(code));
+
+            while (codeExist != null)
+            {
+                code = SCrowdUniqueKey.GetUniqueKey();
+                codeExist = await this.dbContext.User.FirstOrDefaultAsync(u => u.Code.Equals(code));
+            }
+
+            if (exist == null)
+            {
+                User user = new User();
+                user.Email = registerData.Email;
+                user.FacebookUserId = registerResponse.Data.UserId;
+                user.Name = registerResponse.Data.Name;
+                user.UserType = (int)UserType.Facebook;
+                user.ProfilePicture = registerResponse.Data.ProfilePicture;
+                user.AccessToken.Token = registerData.AccessToken;
+                user.Country = registerResponse.Data.Country;
+                user.City = registerResponse.Data.City;
+                user.CountryCode = registerResponse.Data.CountryCode;
+                user.Code = code;
+                user.ColorCode = new UserColorCode().PickColor();
+                var FbUser = await this.dbContext.User.AddAsync(user);
+                return FbUser.Entity;
+            }
+            return exist;
+
+        }
+
+        /// <summary>
+        /// commit and save changes to the db
+        /// only call this from the service, DO NOT CALL FROM REPO ITSELF
+        /// Unit of work methology.
+        /// </summary>
+        public void SaveChanges()
+        {
+            this.dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// saves fcm token
+        /// </summary>
+        /// <param name="userId">id of the user</param>
+        /// <param name="fcmToken">fmc token</param>
+        /// <returns>async task</returns>
+        public async Task SaveFcmToken(int userId, string fcmToken)
+        {
+            FirebaseMessagingToken existingToken = await this.dbContext.FirebaseToken.FirstOrDefaultAsync(token => token.User.Id == userId);
+            if (existingToken == null)
+            {
+                // no token yet saved, insert
+                FirebaseMessagingToken newFcmToken = new FirebaseMessagingToken()
+                {
+                User = await this.GetUserById(userId),
+                Token = fcmToken,
+                };
+                await this.dbContext.FirebaseToken.AddAsync(newFcmToken);
+            }
+            else
+            {
+                // update
+                existingToken.Token = fcmToken;
+                this.dbContext.FirebaseToken.Update(existingToken);
+            }
+        }
     }
-  }
 }
