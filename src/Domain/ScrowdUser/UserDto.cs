7a7761ca6240f1a3aff0414c646fb584a721d1f0
prahveen <prahveent@gmail.com>
2019-10-21 10:57:19 +0530

    send user code

--- src/Domain/ScrowdUser/UserDto.cs
+++ src/Domain/ScrowdUser/UserDto.cs
@@ -1,36 +1,44 @@
 namespace SprintCrowd.BackEnd.Domain.ScrowdUser
 {
-  /// <summary>
-  /// User info data transfer object
-  /// </summary>
-  public class UserDto
-  {
     /// <summary>
-    /// Initalize <see cref="UserDto"> class </see>
+    /// User info data transfer object
     /// </summary>
-    /// <param name="userId">unique id for the user</param>
-    /// <param name="name">name for user</param>
-    /// <param name="profilePicture">profile picture url for user</param>
-    public UserDto(int userId, string name, string profilePicture)
+    public class UserDto
     {
-      this.UserId = userId;
-      this.Name = name;
-      this.ProfilePicture = profilePicture;
+        /// <summary>
+        /// Initalize <see cref="UserDto"> class </see>
+        /// </summary>
+        /// <param name="userId">unique id for the user</param>
+        /// <param name="name">name for user</param>
+        /// <param name="profilePicture">profile picture url for user</param>
+        /// <param name="userCode">profile picture url for user</param>
+        public UserDto(int userId, string name, string profilePicture, string userCode)
+        {
+            this.UserId = userId;
+            this.Name = name;
+            this.ProfilePicture = profilePicture;
+            this.Code = userCode;
+        }
+
+        /// <summary>
+        /// Gets user id
+        /// </summary>
+        public int UserId { get; }
+
+        /// <summary>
+        /// Gets user name
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Gets user profile picture
+        /// </summary>
+        public string ProfilePicture { get; }
+
+        /// <summary>
+        /// Get user coed
+        /// </summary>
+        /// <value></value>
+        public string Code { get; }
     }
-
-    /// <summary>
-    /// Gets user id
-    /// </summary>
-    public int UserId { get; }
-
-    /// <summary>
-    /// Gets user name
-    /// </summary>
-    public string Name { get; }
-
-    /// <summary>
-    /// Gets user profile picture
-    /// </summary>
-    public string ProfilePicture { get; }
-  }
 }
