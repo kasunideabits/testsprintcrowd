75665a13d6b87ad85ba0df8fabe2164a0331317d
prahveen <prahveent@gmail.com>
2019-11-06 12:54:50 +0530

    save changes

--- src/Domain/SprintParticipant/SprintParticipantService.cs
+++ src/Domain/SprintParticipant/SprintParticipantService.cs
@@ -345,6 +345,7 @@
         public async Task RemoveNotification(int notificationId)
         {
             await this.SprintParticipantRepo.RemoveNotification(notificationId);
+            this.SprintParticipantRepo.SaveChanges();
             return;
         }
 
