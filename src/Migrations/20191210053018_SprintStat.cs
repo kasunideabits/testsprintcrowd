ae4b94921fd5778bf29e5ef62946f1accdd1442a
prahveen <prahveent@gmail.com>
2019-12-10 11:36:00 +0530

    sprint stat migrations

---
+++ src/Migrations/20191210053018_SprintStat.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SprintCrowdBackEnd.Migrations
+{
+    public partial class SprintStat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "distance_ran",
+                table: "sprint_participant",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "finish_time",
+                table: "sprint_participant",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "started_time",
+                table: "sprint_participant",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "distance_ran",
+                table: "sprint_participant");
+
+            migrationBuilder.DropColumn(
+                name: "finish_time",
+                table: "sprint_participant");
+
+            migrationBuilder.DropColumn(
+                name: "started_time",
+                table: "sprint_participant");
+        }
+    }
+}
