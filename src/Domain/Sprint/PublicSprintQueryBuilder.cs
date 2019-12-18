e8a7973fc270c499ef6357d7d9e62b34cdd71955
prahveen <prahveent@gmail.com>
2019-12-18 15:03:36 +0530

    filter only public events

--- src/Domain/Sprint/PublicSprintQueryBuilder.cs
+++ src/Domain/Sprint/PublicSprintQueryBuilder.cs
@@ -2,6 +2,7 @@ namespace SprintCrowd.BackEnd.Domain.Sprint
 {
     using System.Linq.Expressions;
     using System;
+    using SprintCrowd.BackEnd.Application;
     using SprintCrowd.BackEnd.Common;
     using SprintCrowd.BackEnd.Infrastructure.Persistence.Entities;
 
@@ -24,11 +25,19 @@ namespace SprintCrowd.BackEnd.Domain.Sprint
 
         public Expression<Func<SprintParticipant, bool>> Build(int offset)
         {
-            Expression<Func<SprintParticipant, bool>> query1 = this.ExtendtedTimeQuery(offset);
-            Expression<Func<SprintParticipant, bool>> query2 = this.DayQyery(offset);
-            Expression<Func<SprintParticipant, bool>> query3 = this.TimeQuery(offset);
-            Expression<Func<SprintParticipant, bool>> query4 = query1.AndAlso(query2);
-            return query4.AndAlso(query3);
+            Expression<Func<SprintParticipant, bool>> query1 = this.PublicSprintQuery();
+            Expression<Func<SprintParticipant, bool>> query2 = this.ExtendtedTimeQuery(offset);
+            Expression<Func<SprintParticipant, bool>> query3 = this.DayQyery(offset);
+            Expression<Func<SprintParticipant, bool>> query4 = this.TimeQuery(offset);
+            Expression<Func<SprintParticipant, bool>> query5 = query1.AndAlso(query2);
+            Expression<Func<SprintParticipant, bool>> query6 = query5.AndAlso(query3);
+            return query6.AndAlso(query4);
+        }
+
+        public Expression<Func<SprintParticipant, bool>> PublicSprintQuery()
+        {
+            Expression<Func<SprintParticipant, bool>> query = s => s.Sprint.Type == (int)SprintType.PublicSprint;
+            return query;
         }
 
         public Expression<Func<SprintParticipant, bool>> ExtendtedTimeQuery(int offset)
