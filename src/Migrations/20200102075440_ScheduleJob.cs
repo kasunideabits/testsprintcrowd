dc204bfa73a9a9a3f33cff988b89b165cd248f51
prahveen <prahveent@gmail.com>
2020-01-02 13:29:00 +0530

    add schedule job migration files

---
+++ src/Migrations/20200102075440_ScheduleJob.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace SprintCrowdBackEnd.Migrations
+{
+    public partial class ScheduleJob : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "schedule_jobs",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    created_date = table.Column<DateTime>(nullable: false),
+                    last_updated = table.Column<DateTime>(nullable: false),
+                    target_id = table.Column<int>(nullable: false),
+                    job_id = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_schedule_jobs", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "schedule_jobs");
+        }
+    }
+}
