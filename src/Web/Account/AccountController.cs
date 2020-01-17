f05e10140f26886853fb2931b023636570b888de
prahveen <prahveent@gmail.com>
2020-01-17 10:14:17 +0530

    signup achievement

--- src/Web/Account/AccountController.cs
+++ src/Web/Account/AccountController.cs
@@ -3,6 +3,7 @@ namespace SprintCrowd.BackEnd.Web.Account
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
     using SprintCrowd.BackEnd.Application;
+    using SprintCrowd.BackEnd.Domain.Achievement;
     using SprintCrowd.BackEnd.Domain.ScrowdUser;
     using SprintCrowd.BackEnd.Extensions;
     using SprintCrowd.BackEnd.Infrastructure.Persistence.Entities;
@@ -15,15 +16,18 @@ namespace SprintCrowd.BackEnd.Web.Account
     public class AccountController : ControllerBase
     {
         private readonly IUserService UserService;
+        private readonly IAchievementService AchievementService;
+
         /// <summary>
         /// No authorization is happening here, just registering a new user.
         /// Sends request to authorization server, authorization server registers and returns user data, those data are saved in the
         /// app database.
         /// Authorization happens in identity server
         /// </summary>
-        public AccountController(IUserService userService)
+        public AccountController(IUserService userService, IAchievementService achievementService)
         {
             this.UserService = userService;
+            this.AchievementService = achievementService;
         }
 
         /// <summary>
@@ -35,6 +39,7 @@ namespace SprintCrowd.BackEnd.Web.Account
         public async Task<IActionResult> Register([FromBody] RegisterModel registerData)
         {
             User user = await this.UserService.RegisterUser(registerData);
+            await this.AchievementService.SignUp(user.Id);
             return this.Ok(new ResponseObject { StatusCode = (int)ApplicationResponseCode.Success, Data = user });
         }
     }
