f5444b02b1933478924a4079ac96f70aa923f580
prahveen <prahveent@gmail.com>
2020-01-17 10:16:47 +0530

    default user  preference

--- src/Infrastructure/Persistence/Entities/UserPreference.cs
+++ src/Infrastructure/Persistence/Entities/UserPreference.cs
@@ -4,18 +4,18 @@ namespace SprintCrowd.BackEnd.Infrastructure.Persistence.Entities
     {
         public int Id { get; set; }
         public int UserId { get; set; }
-        public bool Mon { get; set; } = false;
+        public bool Mon { get; set; } = true;
         public bool Tue { get; set; } = false;
         public bool Wed { get; set; } = false;
-        public bool Thur { get; set; } = false;
+        public bool Thur { get; set; } = true;
         public bool Fri { get; set; } = false;
-        public bool Sat { get; set; } = false;
+        public bool Sat { get; set; } = true;
         public bool Sun { get; set; } = false;
-        public bool Morning { get; set; } = false;
+        public bool Morning { get; set; } = true;
         public bool AfterNoon { get; set; } = false;
-        public bool Evening { get; set; } = false;
+        public bool Evening { get; set; } = true;
         public bool Night { get; set; } = false;
-        public bool TwoToTen { get; set; } = false;
+        public bool TwoToTen { get; set; } = true;
         public bool EleToTwenty { get; set; } = false;
         public bool TOneToThirty { get; set; } = false;
         public virtual User User { get; set; }
