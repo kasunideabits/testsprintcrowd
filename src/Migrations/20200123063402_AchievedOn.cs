496e225fd2425b545c6e4c9873c5b971e38582d1
prahveen <prahveent@gmail.com>
2020-01-23 12:04:24 +0530

    add migrations

---
+++ src/Migrations/20200123063402_AchievedOn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SprintCrowdBackEnd.Migrations
+{
+    public partial class AchievedOn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "achieved_on",
+                table: "notification",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "achieved_on",
+                table: "notification");
+        }
+    }
+}
