56dca70aa2bf507d3e1e583c7f3d5e08167aeb1f
Lasitha <lasitha@ideabits.se>
2020-04-29 08:14:15 +0530

    add new DTO to dashboard joined events

--- src/Domain/SprintParticipant/Dtos/GetSprintDto.cs
+++ src/Domain/SprintParticipant/Dtos/GetSprintDto.cs
@@ -1,10 +1,44 @@
+using System;
 using System.Collections.Generic;
+
 
 namespace SprintCrowd.BackEnd.Domain.SprintParticipant.Dtos
 {
     public class GetSprintDto
     {
-        public SprintInfo Creator { get; set; }
-        public List<SprintInfo> Other { get; set; }
+        public SprintInfoDTO Creator { get; set; }
+        public List<JoinedSprintDTO> Other { get; set; }
+
+    }
+
+    public class JoinedSprintDTO
+    {
+        public SprintInfoDTO SprintInfo { get; set; }
+        public List<ParticipantInfoDTO> ParticipantInfo { get; set; }
+    }
+
+    public class ParticipantInfoDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ColorCode { get; set; }
+
+        public bool IsFriend { get; set; }
+    }
+
+    public class SprintInfoDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Distance { get; set; }
+        public DateTime StartTime { get; set; }
+
+        public DateTime ExtendedTime { get; set; }
+
+        public bool Creator { get; set; }
+
+        public int Type { get; set; }
+
     }
 }
