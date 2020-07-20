77835a5895975f174baba717ccae020265a48f43
Gamith Jayasekara <gamith@ideabits.se>
2020-07-20 14:41:49 +0530

    added migration script related to 869

---
+++ src/Migrations/20200720090316_20200708AddPreviousNotification.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SprintCrowdBackEnd.Migrations
+{
+    public partial class _20200708AddPreviousNotification : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "previous_distance",
+                table: "notification",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "previous_sprint_name",
+                table: "notification",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "previous_start_date",
+                table: "notification",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "previous_distance",
+                table: "notification");
+
+            migrationBuilder.DropColumn(
+                name: "previous_sprint_name",
+                table: "notification");
+
+            migrationBuilder.DropColumn(
+                name: "previous_start_date",
+                table: "notification");
+        }
+    }
+}
