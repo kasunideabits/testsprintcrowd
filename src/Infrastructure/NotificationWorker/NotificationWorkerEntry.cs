8ef87752ddbec6e6486df89796331a9f2bba39cf
Kasun <kasun@ideabits.se>
2020-09-25 05:57:25 +0000

    Update NotificationWorkerEntry.cs

--- src/Infrastructure/NotificationWorker/NotificationWorkerEntry.cs
+++ src/Infrastructure/NotificationWorker/NotificationWorkerEntry.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 namespace SprintCrowd.BackEnd.Infrastructure.NotificationWorker
 {
     using Hangfire.PostgreSql;
@@ -40,59 +39,6 @@ namespace SprintCrowd.BackEnd.Infrastructure.NotificationWorker
 
         }
     }
-    
-    public class MyAuthorizationFilter : IDashboardAuthorizationFilter
-    {
-        public bool Authorize(DashboardContext context)
-        {
-            var httpContext = context.GetHttpContext();
-
-            // Allow all authenticated users to see the Dashboard (potentially dangerous).
-            return true;
-        }
-    }
-    
-=======
-namespace SprintCrowd.BackEnd.Infrastructure.NotificationWorker
-{
-    using Hangfire.Dashboard;
-    using Hangfire.PostgreSql;
-    using Hangfire;
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    /// <summary>
-    /// Hangfire configuration class
-    /// </summary>
-    public static class NotificationWorkerEntry
-    {
-        /// <summary>
-        /// Initialize hangfire
-        /// </summary>
-        public static void Initialize(IConfiguration config, IServiceCollection services)
-        {
-            var notificationWorkerSecsion = config.GetSection("NotificationConfig");
-            var notificationWorkerConfig = notificationWorkerSecsion.Get<NotificationWorkerConfig>();
-            services.AddHangfire(configuration => configuration
-                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
-                .UseSimpleAssemblyNameTypeSerializer()
-                .UseRecommendedSerializerSettings()
-                .UsePostgreSqlStorage(notificationWorkerConfig.HangfireConnection));
-            services.AddHangfireServer();
-        }
-
-        /// <summary>
-        /// Enable dashboard
-        /// </summary>
-        public static void EnableWorkerDashboard(IApplicationBuilder app)
-        {
-            app.UseHangfireDashboard("/hangfire", new DashboardOptions
-            {
-                Authorization = new [] { new MyAuthorizationFilter() }
-            });
-
-        }
-    }
 
     public class MyAuthorizationFilter : IDashboardAuthorizationFilter
     {
@@ -105,5 +51,4 @@ namespace SprintCrowd.BackEnd.Infrastructure.NotificationWorker
         }
     }
 
->>>>>>> qa
 }
